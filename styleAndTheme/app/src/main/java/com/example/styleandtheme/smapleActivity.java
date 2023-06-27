09220333d35cca077023c79584b7225f276f2238
athreya-harshith <athreya1207@gmail.com>
2023-06-27 20:40:11 +0530

    added styles and Theme Implementation

---
+++ styleAndTheme/app/src/main/java/com/example/styleandtheme/smapleActivity.java
@@ -0,0 +1,15 @@
+
+package com.example.styleandtheme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class smapleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smaple);
+    }
+}
