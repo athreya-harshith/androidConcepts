002e500fa57f137155ebc95645e2fd0171f29dd9
athreya-harshith <athreya1207@gmail.com>
2023-06-27 20:44:10 +0530

    added the splash screen implementation

---
+++ splashScreen/app/src/main/java/com/example/splashscreen/splashActivty.java
@@ -0,0 +1,26 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activty);
+
+        Intent intent = new Intent(splashActivty.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
